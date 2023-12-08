e3f3cc656ad5eac1116c6b1460a052a9fd9a432e
Goutham-BL <goutham.y@bridgelabz.com>
2023-12-08 12:08:11 +0530

    Added : Hello UC1 Branch

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ package com.demo;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello this my sample program");
+        System.out.println("Hello UC1 branch");
     }
 }
